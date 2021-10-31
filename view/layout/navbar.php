d582457e068da5606d286f009bf44595fa2d40ac
Gabriela Cardoso <gabrielacardososilva9@gmail.com>
2021-10-30 23:02:33 -0300

    update

--- view/layout/navbar.php
+++ view/layout/navbar.php
@@ -10,7 +10,7 @@
     <ul>
         <li><a href="#" class="page calendar">Agendas<span class="material-icons button-calendar">arrow_right</span></a>
             <ul class="subpage subcalendar">
-                <li><a href="#">Cadastrar agendas</a></li>
+                <li><a href="#">Agendar consulta</a></li>
                 <li><a href="#">Consultar agendas</a></li>
                 <li><a href="#">Alterar agendas</a></li>
                 <li><a href="#">Imprimir agendas</a></li>
---
+++ view/page/agenda_create.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../assets/css/reset.css">
+    <link rel="stylesheet" href="../../assets/css/navbar.css">
+    <link rel="stylesheet" href="../../assets/css/grid.css">
+    <link rel="stylesheet" href="../../assets/css/agenda_create.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+    <title>Agendamento</title>
+</head>
+<body>
+
+    <div class="grid-container">
+
+        <div class="navbar">
+            <?php include('../layout/navbar.php')?>
+        </div>
+
+        <div class="title">
+            <a href="#">
+                <span class="material-icons help">help</span>
+            </a>
+            <h1 class="h1-title">Agendar consultas</h1>
+        </div>
+        
+        <div class="info">
+            <p class="text-info">Para agendar novas consultas, preencha os campos abaixo e clique em salvar.</p>
+        </div>
+        
+        <!-- <form action="" class="form"> -->
+            <div class="bodyl"> 
+                <div class="alignl">
+                    <div class="single-input">
+                        <input type="numbers" name="input_cpf" class="input" id="cpf" required>
+                        <label for="specialty" class="label">CPF</label>
+                    </div>
+
+                    <div class="single-input">
+                        <input type="text" name="input_name" class="input" id="name" required>
+                        <label for="cpf" class="label">Nome completo</label>
+                    </div>
+
+                    <div class="single-input">
+                        <input type="text" name="input_specialty" class="input" id="specialty" required>
+                        <label for="specialty" class="label">Especialidade</label>
+                    </div>
+
+                    <div class="single-input">
+                        <input type="text" name="input_doctor" class="input" id="doctor" required>
+                        <label for="doctor" class="label">Médico</label>
+                    </div>
+                </div>          
+            </div>
+
+            <div class="bodyr">
+                <div class="alignr">
+                    <div class="single-input">
+                        <input type="date" name="input_date" class="input" id="date" required>
+                        <!-- <label for="date" class="label">Data</label> -->
+                    </div>
+
+                    <div class="single-input">
+                        <input type="time" name="input_hour" class="input" id="hour" required>
+                        <!-- <label for="hour" class="label">horário</label> -->
+                    </div>
+
+                    <div class="single-input">
+                        <input type="number" name="input_number" class="input" id="number" required>
+                        <label for="number" class="label">Número do consultório</label>
+                    </div>
+
+                    <div class="single-input">
+                        <input type="radio" name="type" id="first" value="Primeira consulta">
+                        <label for="first">Primeira consulta</label>
+                        <input type="radio" name="type" id="return" value="Retorno">
+                        <label for="return">Retorno</label>
+                    </div>
+                </div>
+            </div>
+            
+            
+            <div class="footer">
+                <button type="submit" class="center">Salvar</button>
+                <button type="reset" class="center">Limpar</button>
+            </div>
+        <!-- </form> -->
+            
+    </div>
+
+
+    <script type="text/javascript" src="../../assets/js/navbar.js"></script>
+</body>
+</html>
+
+                <!-- <div class="single-input">
+                    <label for="opt_type">Tipo de consulta</label>
+                    <select name="opt_type" id="opt_type">
+                        <option value="return">Retorno</option>
+                        <option value="first">Primeria consulta</option>
+                    </select>
+                </div> -->
+                <!-- <div class="single-input">
+                    <input type="number" name="input_price" class="input" id="price" required>
+                        <label for="price">Valor????</label>
+                    </div> -->
+
+            
+                <!-- <div class="single-input">
+                    <input type="text" name="input_comments" class="input" id="comments">
+                    <label for="comments">Observações</label>
+                </div> -->
+
+
+
+
+
+                <!-- <div class="single-input-search">
+            <input type="search" class="input" id="cpf" required>
+            <label for="cpf">Pesquisa por CPF (apenas números)</label>
+        </div> -->
